c184f53e169c35876078d7688fe312a9341c41da
Spencer Stock <spencerstock@gmail.com>
2019-04-29 09:58:54 -0600

    added logic to check if second click and first click are always lining up

--- app/src/main/java/com/example/intermediatejavachallenge/DoubleClickHandler.java
+++ app/src/main/java/com/example/intermediatejavachallenge/DoubleClickHandler.java
@@ -4,18 +4,25 @@ import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.view.View;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class DoubleClickHandler implements DoubleClickInterface {
+    private static final int DOUBLE_CLICK_TIMER = 600;
+
     public  View                view;
     private DoubleClickListener doubleClickListener;
-    private static final int DOUBLE_CLICK_TIMER = 500;
-    private int clicks;
-    private Drawable defaultBackground;
+    private int                 clicks;
+    private Drawable            defaultBackground;
+    private Semaphore lock;
+    private static int sleepingThreads = 0;
 
 
     public DoubleClickHandler(View view) {
         this.view = view;
         defaultBackground = view.getBackground();
         clicks = 0;
+        lock = new Semaphore(1);
     }
 
     @Override
@@ -24,7 +31,7 @@ public class DoubleClickHandler implements DoubleClickInterface {
     }
 
     public void processClick() {
-        new Thread(new Runnable() {
+        new Thread(new Runnable() { //every click that comes in is a new thread
             @Override
             public void run() {
                 clicks++;
@@ -37,11 +44,18 @@ public class DoubleClickHandler implements DoubleClickInterface {
                                 view.setBackgroundColor(view.getResources().getColor(R.color.colorButtonPress));
                             }
                         });
+                        sleepingThreads++;
                         Thread.sleep(DOUBLE_CLICK_TIMER);
+                        sleepingThreads--;
+                        if (sleepingThreads != 0) return;
+                        lock.acquire();
                     }
-                    if (clicks > 1) sendClickAction(true);
+                    if (clicks > 1) {
+                        sendClickAction(true);
+                    }
                     else if (clicks == 1) sendClickAction(false);
                     clicks = 0;
+                    lock.release();
                     activity.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
